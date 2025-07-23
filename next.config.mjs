8e1d20e949b670ef6e0ebd1bf4302c0c81453797
MATE-AI-TEAM <team@aimate.me>
2025-07-23 22:06:51 +0800

    ✨ feat(next.config): add experimental MDX loader support

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const withNextra = nextra({});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui", "web"],
+  experimental: {
+    turbo: {
+      loaders: {
+        ".mdx": ["@mdx-js/loader"],
+      }
+    }
+  },
   webpack: (config, { dev }) => {
     if (!dev) {
       config.module.rules.push({
