3494e23367a7e3d5250d41f03a24d7df6e05fb27
MATE-AI-TEAM <team@aimate.me>
2025-07-23 22:00:57 +0800

    ♻️ refactor: add MDX support in webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ const withNextra = nextra({});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui", "web"],
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.module.rules.push({
+        test: /\.mdx?$/,
+        use: [
+          {
+            loader: "@mdx-js/loader",
+            /** @type {Options} */
+            // options: {/* jsxImportSource: …, otherOptions… */}
+          },
+        ],
+      });
+    }
+
+    return config;
+  },
 };
 
 export default withNextra(nextConfig);
